d4a5fe547d5ccd76a44b3d5bc60847e207a871c6
farrosghulam <farrosghulam@gmail.com>
2026-09-18 08:58:32 +0700

    displaying enemy wave 1 to 100

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i <= 100; i++)
+{
+    Console.WriteLine($"Enemy wave {i} has started!");
+    Console.WriteLine($"Enemy wave {i} is cleared!\n");
+}
